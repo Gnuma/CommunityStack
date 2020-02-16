bd5743b38e61ddc1d8183f30bb9ca86a261a058e
ihategoto <cdemilio80@gmail.com>
2020-02-16 18:00:40 +0100

    TopicHandler registered in urls

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import CategoryHandler
+from .views import CategoryHandler, TopicHandler
 
 router = DefaultRouter()
 router.register('categories', CategoryHandler)
+router.register('topics', TopicHandler)
 
 urlpatterns = [
 ]
