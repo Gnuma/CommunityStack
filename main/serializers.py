325ea5cb11493f7deabd7900136571175ec32c24
ihategoto <cdemilio80@gmail.com>
2020-02-16 17:59:33 +0100

    TopicHandler class

--- main/serializers.py
+++ main/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Category
+from .models import Category, Topic
 
 class CategorySerializer(ModelSerializer):
-    class Meta():
+    class Meta:
         fields = '__all__'
         model = Category
+
+
+class TopicSerializer(ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Topic
--- main/views.py
+++ main/views.py
@@ -1,9 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny
 from django.http import JsonResponse
-from .serializers import CategorySerializer
-from .models import Category
+from .serializers import CategorySerializer, TopicSerializer
+from .models import Category, Topic
+
 
 class CategoryHandler(ModelViewSet):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+
+
+class TopicHandler(ModelViewSet):
+    serializer_class = TopicSerializer
+    queryset = Topic.objects.all()
